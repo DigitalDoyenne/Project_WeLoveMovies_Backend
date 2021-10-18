61136fc75fda2ef7f10cc861536cbc0da0f3e8d4
digitaldoyenne <digitaldoyenne@protonmail.com>
2021-10-18 16:17:19 -0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+if (process.env.USER) require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler")
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+//Error Handlers
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
